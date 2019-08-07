ba95db17a0822342c79991f4334168be7efd5a11
Adrià Fontcuberta <afontcu@gmail.com>
2019-08-07 19:49:16 +0200

    Configure basic textlint

---
+++ website/.textlintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    apostrophe: true,
+    '@textlint-rule/no-unmatched-pair': true,
+    'common-misspellings': true,
+    'stop-words': true,
+    'write-good': true,
+  },
+}
