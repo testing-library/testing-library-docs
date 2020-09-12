0a1afe65851c0af1597c6ad5f85be39c1daa76fd
Matan Borenkraout <matanbobi@gmail.com>
2020-09-12 13:58:05 +0300

    update gitignore

--- website/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/matan/Dev/testing-library-docs/website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/matan/Dev/testing-library-docs/website/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/matan/Dev/testing-library-docs/website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/matan/Dev/testing-library-docs/website/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/matan/Dev/testing-library-docs/website/src/css/custom.css"),
-];
