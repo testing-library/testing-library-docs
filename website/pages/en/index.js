80e4ebad874605239d10f019e8a5313c88207d58
Alex Krolick <alexkrolick@users.noreply.github.com>
2020-10-01 15:29:03 -0700

    shorten adapter names to remove line wraps (#631)

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -187,65 +187,65 @@ class Index extends React.Component {
             image: `${baseUrl}img/react-128x128.png`,
             imageAlign: 'top',
             title:
-              '[React Testing Library](./docs/react-testing-library/intro)',
+              '[React](./docs/react-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/cypress-128x128.png`,
             imageAlign: 'top',
             title:
-              '[Cypress Testing Library](./docs/cypress-testing-library/intro)',
+              '[Cypress](./docs/cypress-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/testcafe-128x128.jpg`,
             imageAlign: 'top',
-            title: `[Testcafe Testing Library](./docs/testcafe-testing-library/intro)`,
+            title: `[Testcafe](./docs/testcafe-testing-library/intro)`,
           },
           {
             image: `${baseUrl}img/svelte-logo.png`,
             imageAlign: 'top',
             title:
-              '[Svelte Testing Library](./docs/svelte-testing-library/intro)',
+              '[Svelte](./docs/svelte-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/vue-400x400.png`,
             imageAlign: 'top',
-            title: '[Vue Testing Library](./docs/vue-testing-library/intro)',
+            title: '[Vue](./docs/vue-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/angular-250x250.png`,
             imageAlign: 'top',
             title:
-              '[Angular Testing Library](./docs/angular-testing-library/intro)',
+              '[Angular](./docs/angular-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/reason-200x200.png`,
             imageAlign: 'top',
             title:
-              '[ReasonReact Testing Library](./docs/bs-react-testing-library/intro)',
+              '[ReasonReact](./docs/bs-react-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/puppeteer-275x275.png`,
             imageAlign: 'top',
             title:
-              '[Puppeteer Testing Library](./docs/pptr-testing-library/intro)',
+              '[Puppeteer](./docs/pptr-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/react-native-128x128.png`,
             imageAlign: 'top',
             title:
-              '[React Native Testing Library](./docs/react-native-testing-library/intro)',
+              '[React Native](./docs/react-native-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/preact-128x128.png`,
             imageAlign: 'top',
             title:
-              '[Preact Testing Library](./docs/preact-testing-library/intro)',
+              '[Preact](./docs/preact-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/nightwatch-128x128.png`,
             imageAlign: 'top',
             title:
-              '[Nightwatch Testing Library](./docs/nightwatch-testing-library/intro)',
+              '[Nightwatch](./docs/nightwatch-testing-library/intro)',
           },
           {
             image: `${baseUrl}img/construction-128x128.png`,
