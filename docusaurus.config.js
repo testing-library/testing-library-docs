cf466ea9b85ab2209dd4949c5cb91951ad2e3b92
Matan Borenkraout <matanbobi@gmail.com>
2020-11-03 09:36:47 +0200

    pull the website directory outside

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -218,7 +218,7 @@ module.exports = {
           showLastUpdateTime: true,
           editUrl:
             'https://github.com/testing-library/testing-library-docs/blob/master/docs/',
-          path: '../docs',
+          path: './docs',
           sidebarPath: './sidebars.js',
         },
         blog: {
