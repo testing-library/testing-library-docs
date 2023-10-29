bfbf510d7e10bd3d461d3c727827c4e06db7284a
Nick McCurdy <nick@nickmccurdy.com>
2023-10-28 20:02:27 -0400

    chore: format

--- src/components/Showcase/index.jsx
+++ src/components/Showcase/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import {useThemeConfig} from '@docusaurus/theme-common';
+import {useThemeConfig} from '@docusaurus/theme-common'
 
 export const Showcase = ({users}) => {
-    const {isDarkTheme} = useThemeConfig()
-    return users.map(user => (
-        <a href={user.infoLink} key={user.infoLink}>
-            <img
-                src={isDarkTheme ? user.darkImage : user.lightImage}
-                alt={user.caption}
-                title={user.caption}
-            />
-        </a>
-    ))
+  const {isDarkTheme} = useThemeConfig()
+  return users.map(user => (
+    <a href={user.infoLink} key={user.infoLink}>
+      <img
+        src={isDarkTheme ? user.darkImage : user.lightImage}
+        alt={user.caption}
+        title={user.caption}
+      />
+    </a>
+  ))
 }
